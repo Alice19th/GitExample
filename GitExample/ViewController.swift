4f132426330077dd5d52ef3b138a0f64fd62d583
Alice Ma <s3634707@rmit.edu.vn>
2018-08-28 11:29:00 +0700

    Initial Commit

---
+++ GitExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitExample
+//
+//  Created by Anh, Ma Ngoc Phuong on 8/28/18.
+//  Copyright © 2018 Anh, Ma Ngoc Phuong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
